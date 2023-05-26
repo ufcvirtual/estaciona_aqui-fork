7ab4d69377158e51152a38e9f020ffe8331d7260
Pedro Arruda <pedrooliveira2702@gmail.com>
2023-05-26 15:42:12 -0300

    <commit_message>

---
+++ login/codigo.js
@@ -0,0 +1,21 @@
+function ver(){
+var code = document.getElementById('senha').value;
+var aviso=document.getElementById('alerta');
+var valor = code.length;
+var embora =document.getElementById('buttao')
+if(code=="senha"){
+  alert("Você entrou")
+  aviso.style.display="none"
+window.location.href="/Seleção de vagas/index.html";
+}else if(valor==0){
+    aviso.style.display="flex"
+    aviso.innerHTML="* Digite uma senha"
+
+} else if(!(code=="senha")){
+    aviso.style.display="flex"
+    aviso.innerHTML="*  A Senha está errada contate o suporte."
+    
+
+}
+  
+}
